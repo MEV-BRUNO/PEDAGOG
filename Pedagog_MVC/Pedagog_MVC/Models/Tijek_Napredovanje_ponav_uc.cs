e740e07ca172ccb2cb0a2b49972f4b77c872411c
Matej <Matej@DESKTOP-C3NQ0DS>
2018-04-24 11:53:52 +0200

    Modeli

---
+++ Pedagog_MVC/Pedagog_MVC/Models/Tijek_Napredovanje_ponav_uc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Pedagog_MVC.Models;
+
+namespace Pedagog_MVC.Models
+{
+    public class Tijek_Napredovanje_ponav_uc
+    {
+        public int id_napredovanje { get; set; }
+        public int id_tijek { get; set; }
+        public DateTime datum { get; set; }
+        public string sadrzaj { get; set; }
+        public string dogovor { get; set; }
+
+    }
+}
