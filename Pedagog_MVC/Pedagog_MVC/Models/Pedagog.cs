851a8a9958864d5a6f361524f58e994830dd1347
TiborN <38249269+TiborN@users.noreply.github.com>
2018-04-24 21:18:14 +0200

    Modeli za bazu

---
+++ Pedagog_MVC/Pedagog_MVC/Models/Pedagog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pedagog_MVC.Models
+{
+    public class Pedagog
+    {
+        public int id_pedagog { get; set; }
+        public string ime_prezime { get; set; }
+        public string mail { get; set; }
+        public string lozinka { get; set; }
+        public string titula { get; set; }
+        public DateTime licenca { get; set; }
+        public string aktiv_link { get; set; }
+        public int aktivan { get; set; }
+        public int id_skola { get; set; }
+    }
+}
---
+++ Pedagog_MVC/Pedagog_MVC/Models/Roditelj_biljeske_mjesec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pedagog_MVC.Models
+{
+    public class Roditelj_biljeske_mjesec
+    {
+        public long id_biljeske { get; set; }
+        public string mjesec { get; set; }
+        public string zakljucak { get; set; }
+    }
+}
