a8fba82da58d864fe394abeab524148f76c39315
MHrncic <marko.hrncic@mev.student.hr>
2018-05-27 17:02:47 +0200

    Izrada funkcionalnosti viewa za Adresar, napravljene CRUD funkcije u kontroleru za Adresar uz pomoc modala i sve povezano s bazom.

--- Pedagog_MVC/Pedagog_MVC/BazaPovezivanje/BazaDbContext.cs
+++ Pedagog_MVC/Pedagog_MVC/BazaPovezivanje/BazaDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using Pedagog_MVC.Models;
+using ProjektIdio.Models;
 
 namespace Pedagog_MVC.BazaPovezivanje
 {
@@ -11,6 +12,9 @@ namespace Pedagog_MVC.BazaPovezivanje
     {
 
         public DbSet<Pedagog> Pedagozi { get; set; }
+        public DbSet<Ucenik> Ucenici { get; set; }
+
+        public DbSet<Adresar> Adresari { get; set; }
 
     }
 }
